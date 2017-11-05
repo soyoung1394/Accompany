5cedb20a25e06127e5798d5c9283df6d30fae3ba
syoung <syoung1394@naver.com>
2017-11-05 17:20:44 +0900

    second

--- app/src/main/java/sinabro/itistest/MainActivity.java
+++ app/src/main/java/sinabro/itistest/MainActivity.java
@@ -1,7 +1,12 @@
 package sinabro.itistest;
 
-import android.support.v7.app.AppCompatActivity;
+import android.app.Activity;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.view.animation.Animation;
+import android.view.animation.AnimationUtils;
+import android.widget.ImageView;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,4 +15,21 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
     }
-}
+
+        Animation anim;
+
+        @Override
+        protected void onCreate(Bundle savedInstanceState) {
+            super.onCreate(savedInstanceState);
+            setContentView(R.layout.activity_main);
+        }
+        ImageView iv=(ImageView)findViewById(R.id.imageView);
+        public void rotate(View v) {
+            anim = AnimationUtils.loadAnimation(this,R.anim.rotate);
+            iv.startAnimation(anim);
+        }
+    }
+
+
+
+
